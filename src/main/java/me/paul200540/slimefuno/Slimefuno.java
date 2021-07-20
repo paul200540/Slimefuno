10f467b51662d07333893c763abab40429cbaf12
paul200540 <poker2000522@gmail.com>
2021-07-20 15:15:51 -0230

    test

--- src/main/java/me/paul200540/slimefuno/Slimefuno.java
+++ src/main/java/me/paul200540/slimefuno/Slimefuno.java
@@ -1,11 +1,5 @@
 package me.paul200540.slimefuno;
 
-import me.mrCookieSlime.Slimefun.cscorelib2.skull.SkullItem;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
@@ -13,6 +7,12 @@ import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
 import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
+import me.mrCookieSlime.Slimefun.cscorelib2.skull.SkullItem;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class Slimefuno extends JavaPlugin implements SlimefunAddon {
 
@@ -30,21 +30,19 @@ public class Slimefuno extends JavaPlugin implements SlimefunAddon {
          * This Category will use the following ItemStack
          */
         ItemStack categoryItem = new CustomItem(SkullItem.fromHash("4934a9f5ab1789a7d8dd96d32493cdacff577d8c81e7b23917dff2e32bd0bc10"), "&a&lSlimefuno", "", "&a> Click to open");
-        ItemStack categoryItem2 = new CustomItem(SkullItem.fromHash("931b6c35a8ac8366a4db51b042a8728dd2e8e5f2fe44aff08fcf5af3b921c6dd"), "&a&lSlimefuno &2&lResources", "", "&a> Click to open");
 
         // Give your Category a unique id.
         NamespacedKey categoryId = new NamespacedKey(this, "Slimefuno");
-        NamespacedKey categoryId2 = new NamespacedKey(this, "Slimefuno Resources");
         Category category = new Category(categoryId, categoryItem);
-        Category category2 = new Category(categoryId2, categoryItem2);
 
         /*
          * 2. Create a new SlimefunItemStack
          * This class has many constructors, it is very important
          * that you give each item a unique id.
          */
-        SlimefunItemStack slimefunItem = new SlimefunItemStack("CLEAR_SLIME", Material.LIME_STAINED_GLASS, "&a&lClear &2&lSlime", "&a&lSlime that's clear??? Woah!");
-
+        SlimefunItemStack CLEAR_SLIME = new SlimefunItemStack("CLEAR_SLIME", Material.LIME_STAINED_GLASS, "&a&lClear &2&lSlime", "&a&lSlime that's clear??? Woah!");
+        SlimefunItemStack CONDENSED_SLIME = new SlimefunItemStack("CONDENSED_SLIME", Material.SLIME_BLOCK, "&a&lCondensed &2&lSlime", "&a&lIt's so condensed that you", "&a&lcan't even see through it!");
+        CLEAR_SLIME.addUnsafeEnchantment(Enchantment.SILK_TOUCH, 5);
         /*
          * 3. Creating a Recipe
          * The Recipe is an ItemStack Array with a length of 9.
@@ -52,8 +50,8 @@ public class Slimefuno extends JavaPlugin implements SlimefunAddon {
          * The machine in which this recipe is crafted in is specified
          * further down as the RecipeType.
          */
-        ItemStack[] recipe = { new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.GLASS), new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.SLIME_BLOCK) };
-
+        ItemStack[] ClearSlimeRecipe = { new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.GLASS), new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.SLIME_BLOCK), new ItemStack(Material.SLIME_BLOCK) };
+        ItemStack[] CondensedSlimeRecipe = {CLEAR_SLIME, CLEAR_SLIME, CLEAR_SLIME, CLEAR_SLIME, CLEAR_SLIME, CLEAR_SLIME, CLEAR_SLIME, CLEAR_SLIME, CLEAR_SLIME};
         /*
          * 4. Registering the Item
          * Now you just have to register the item.
@@ -61,8 +59,10 @@ public class Slimefuno extends JavaPlugin implements SlimefunAddon {
          * which this item is crafted in.
          * Recipe Types from Slimefun itself will automatically add the recipe to that machine.
          */
-        SlimefunItem item = new SlimefunItem(category2, slimefunItem, RecipeType.MAGIC_WORKBENCH, recipe);
+        SlimefunItem item = new SlimefunItem(category, CLEAR_SLIME, RecipeType.MAGIC_WORKBENCH, ClearSlimeRecipe);
+        SlimefunItem item2 = new SlimefunItem(category, CONDENSED_SLIME, RecipeType.MAGIC_WORKBENCH, CondensedSlimeRecipe);
         item.register(this);
+        item2.register(this);
     }
 
     @Override
